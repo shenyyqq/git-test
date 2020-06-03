c461a3d6d3b64310887e0ef8109098eb7089900a
shenyingqi <17600852887@163.com>
2020-06-03 14:11:49 +0800

    分支添加了一个类

---
+++ git-test-project/src/com/shenyingqi/Branch.java
@@ -0,0 +1,13 @@
+package com.shenyingqi;
+
+/**
+ * @program: git-test-project
+ * @description: TODO
+ * @author： shenyingqi
+ * @create: 2020-06-03-14:10
+ **/
+public class Branch {
+    public static void main(String[] args) {
+        System.out.println("新建了一个分支");
+    }
+}
