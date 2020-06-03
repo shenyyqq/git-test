ebb45c2d2470924ed1302c0e7b5cc2971660a0f2
shenyingqi <17600852887@163.com>
2020-06-03 14:13:47 +0800

    主支添加了一个类

---
+++ git-test-project/src/com/shenyingqi/MasterC.java
@@ -0,0 +1,13 @@
+package com.shenyingqi;
+
+/**
+ * @program: git-test-project
+ * @description: TODO
+ * @author： shenyingqi
+ * @create: 2020-06-03-14:13
+ **/
+public class MasterC {
+    public static void main(String[] args) {
+        System.out.println("master创建");
+    }
+}
