eef7e9ce1a360e3974c8cd6c6b085986e8bb4fdf
shenyingqi <17600852887@163.com>
2020-06-03 13:50:23 +0800

    新加入了GitTet.java

---
+++ git-test-project/src/com/shenyingqi/GitTet.java
@@ -0,0 +1,18 @@
+package com.shenyingqi;
+
+/**
+ * @program: git-test-project
+ * @description: TODO
+ * @author： shenyingqi
+ * @create: 2020-06-03-13:45
+ **/
+public class GitTet {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("修改了git");
+        System.out.println("hhhh");
+    }
+}
